6d73f4e1aceddf27e8f1a314d959fc5538bf5d13
AppInstaller iOS <98632439+BenjaminHornbeck6@users.noreply.github.com>
2025-03-12 07:15:31 -0400

    Minor changes

--- stage1/main.c
+++ stage1/main.c
@@ -10,32 +10,6 @@ void print(char* message);
 void printHex(char* message, uint64_t value);
 char* combineStrings(char* str1, char* str2);
 
-id stringWithCString(char* string) {
-    Class class = objc_getClass("NSString");
-    return class ? ((id(*)(Class, SEL, char*))objc_msgSend)(class, sel_registerName("stringWithCString:"), string) : NULL;
-}
-
-id defaultManager(void) {
-    Class class = objc_getClass("NSFileManager");
-    return class ? ((id(*)(Class, SEL))objc_msgSend)(class, sel_registerName("defaultManager")) : NULL;
-}
-
-id contentsOfDirectoryAtPath(id fileManager, id path) {
-    return ((id(*)(id, SEL, id, id))objc_msgSend)(fileManager, sel_registerName("contentsOfDirectoryAtPath:error:"), path, NULL);
-}
-
-id getDescription(id object) {
-    return ((id(*)(id, SEL))objc_msgSend)(object, sel_registerName("description"));
-}
-
-char* UTF8String(id string) {
-    return ((char*(*)(id, SEL))objc_msgSend)(string, sel_registerName("UTF8String"));
-}
-
-void printNSObject(id object) {
-    print(UTF8String(getDescription(object)));
-}
-
 int main(void) {
     initializeSymbols();
     
@@ -48,17 +22,6 @@ int main(void) {
     printHex("sel_registerName: ", (uint64_t)sel_registerName);
     printHex("objc_msgSend: ", (uint64_t)objc_msgSend);
     
-    char* homePath = getenv("HOME");
-    char* path = combineStrings(homePath, "/Library/Caches/com.apple.WebKit.WebContent");
-    
-    id pathNSString = stringWithCString(path);
-    printNSObject(pathNSString);
-    
-    id fileManager = defaultManager();
-    printNSObject(fileManager);
-    
-    id contents = contentsOfDirectoryAtPath(fileManager, pathNSString);
-    printNSObject(contents);
     
     sleep(60);
     return 0;
